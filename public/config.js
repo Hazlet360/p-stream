550f4b108207ce3b84d27c8d011d4a142f8b059b
Hazlet360 <crabtree12@gmail.com>
2025-07-28 21:53:44 +0100

    Update config.js

--- public/config.js
+++ public/config.js
@@ -4,7 +4,7 @@ window.__CONFIG__ = {
   VITE_CORS_PROXY_URL: "",
 
   // The READ API key to access TMDB
-  VITE_TMDB_READ_API_KEY: "",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZTJkNWQxY2M1NTUzYzI4NjVjYzU4MDhjZTA2YjNlZiIsIm5iZiI6MTc1MzczMzQ2Ni40MjQsInN1YiI6IjY4ODdkOTVhZTA4OGQ1NzhjNzhhNzgzNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.287iwHpuDEPlbC-r9NHWUfPhngdi-5J2uirgi5nM9Ac",
 
   // The DMCA email displayed in the footer, null to hide the DMCA link
   VITE_DMCA_EMAIL: null,
